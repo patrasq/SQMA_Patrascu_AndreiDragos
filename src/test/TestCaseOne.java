421054f4e31350cb1ca60ba8217e000184c4552d
Andrei-Dragos Patrascu <andreipatrasq@yahoo.com>
2024-12-15 18:20:23 +0200

    Initial commit

---
+++ src/test/TestCaseOne.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestCaseOne {
+    @Test
+    public void testExampleOne() {
+        assertTrue(1 == 1);
+    }
+}
---
+++ src/test/TestCaseTwo.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class TestCaseTwo {
+    @Test
+    public void testExampleOne() {
+        assertFalse(1 == 2);
+    }
+}
